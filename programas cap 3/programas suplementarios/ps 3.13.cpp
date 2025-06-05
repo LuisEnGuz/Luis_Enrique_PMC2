3511d5261fd796bce7e69aeca110ddd5d80bc797
LuisEnGuz <wbaez2905@gmail.com>
2025-06-05 00:06:01 -0700

    Create ps 3.13.cpp

---
+++ programas cap 3/programas suplementarios/ps 3.13.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int N, I, J, PRI;
+    printf("\nIngrese el Limite de los primos");
+    scanf("%i", &N);
+    for (I = 2; I <= N; I++) // empieza con 2 porque 1 no es primo
+    { 
+        PRI = 1;
+        for (J = 2; J < I; J++) 
+        {
+            if (I % J == 0) 
+            {
+                PRI = 0;
+                break;
+            }
+             if ((I + 2)% J == 0) 
+            {
+                PRI = 0;
+                break;
+            }
+        }
+        if (PRI) {
+            printf("\nPrimos gemelos:%i %i", I, I+2);
+        }
+    }
+    return(0);
+}
