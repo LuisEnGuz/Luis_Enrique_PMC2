a1f8484dd4f4fb3c17e682ba26398ec4f1415085
LuisEnGuz <wbaez2905@gmail.com>
2025-06-05 18:40:42 -0700

    Update ps 3.17.cpp

--- programas cap 3/programas suplementarios/ps 3.17.cpp
+++ programas cap 3/programas suplementarios/ps 3.17.cpp
@@ -6,11 +6,14 @@ int main() {
     printf("Ingrese un numero entero positivo: ");
     scanf("%d", &N);
 
-    for (I = 1; I <= N; I++;
-        for (J = 1; J <= I; J++) {
+    for (I = 1; I <= N; I++)
+        {
+        for (J = 1; J <= I; J++)
+        {
             printf("%d ", J);
         }
-        for (J = I - 1; J >= 1; J--) {
+        for (J = I - 1; J >= 1; J--) 
+        {
             printf("%d ", J);
         }
         printf("\n");
