884f4539ecf24368eb4d4a70d92e4b6df3f6c154
LuisEnGuz <wbaez2905@gmail.com>
2025-07-02 09:00:42 -0700

    Create programa 7.3.cpp

---
+++ programas 7/programa 7.3.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    char* cad0;
+    cad0 = "Argentina";
+    puts (cad0);
+
+    cad0 = "brasil";
+    puts (cad0);
+    char *cad1;
+    gets (cad1);
+
+    char *cad1= "";
+    gets (cad1);
+
+    char cad1[9]; //le puse el numero porque el error me estaba desesperando
+    
+    char cad2[20] = "mexico";
+    puts (cad2);
+    gets(cad2);
+
+    char cad[10]= "Guatemala";
+}
