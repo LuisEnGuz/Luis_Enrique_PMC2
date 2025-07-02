396f7e69f2250f28ea3d4f4f34c659fe78bae7de
LuisEnGuz <wbaez2905@gmail.com>
2025-07-02 13:38:44 -0700

    Create 9.2.cpp

---
+++ programas cap 9/9.2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char p1;
+    FILE *ar;
+    if ((ar = fopen("arc.txt","r")) != NULL)
+    {
+        while (!feof(ar))
+        {
+            p1 = fgetc(ar);
+            putchar(p1);
+        }
+        fclose(ar);
+    }
+    else
+    {
+        printf("El archivo no se puede abrir");
+    }
+}
