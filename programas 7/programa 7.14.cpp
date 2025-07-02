ee506dc67c67d0a00d06d74543dd63bd5784a75e
LuisEnGuz <wbaez2905@gmail.com>
2025-07-02 10:15:08 -0700

    Create programa 7.14.cpp

---
+++ programas 7/programa 7.14.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int interpreta(char *);
+ int main()
+ {
+    char cad[50];
+    printf("\nIngrese la cadena de caracteres: ");
+    gets(cad);
+    interpreta(cad);
+ }
+ int interpreta(char *cad)
+ {
+    int i = 0, j, k;
+    while (cad[i] !='\0')
+    {
+        if(isalpha (cad[i]))
+        {
+            k = cad[ i - 1] - 48;
+            for (j = 0; j < k; j ++)
+                 putchar(cad[i]);
+        }
+        i++;
+    }    
+ }
