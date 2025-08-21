77274cc43dc4cfdf8feb4d5ef722a7b7d3cfb2a3
LuisEnGuz <wbaez2905@gmail.com>
2025-08-21 13:58:21 -0700

    Update bubble sort.cpp

--- projectos/bubble sort.cpp
+++ projectos/bubble sort.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-#include <cstring>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 typedef struct {
     int MATRICULA;
@@ -17,19 +16,19 @@ int main() {
     int TAM;
 
     do {
-        cout << "Ingrese el tamaño del arreglo (1-50): ";
-        cin >> TAM;
+        printf("Ingrese el tamaño del arreglo (1-50): ");
+        scanf("%d", &TAM);
     } while (TAM < 1 || TAM > 50);
 
-    cin.ignore(); // limpiar buffer
+    getchar(); // limpiar buffer
     LECTURA(ARRE, TAM);
 
-    cout << "\nAntes de ordenar:\n";
+    printf("\nAntes de ordenar:\n");
     imprimir(ARRE, TAM);
 
     bubbleSort(ARRE, TAM);
 
-    cout << "\nDespués de ordenar por nombre:\n";
+    printf("\nDespués de ordenar por nombre:\n");
     imprimir(ARRE, TAM);
 
     return 0;
@@ -37,32 +36,36 @@ int main() {
 
 int LECTURA(ALUMNO A[], int T) {
     for (int i = 0; i < T; i++) {
-        cout << "\nIngrese los datos del alumno " << i + 1 << ":\n";
+        printf("\nIngrese los datos del alumno %d:\n", i + 1);
 
-        cout << "Matrícula: ";
-        cin >> A[i].MATRICULA;
-        cin.ignore();
+        printf("Matrícula: ");
+        scanf("%d", &A[i].MATRICULA);
+        getchar();
 
-        cout << "Nombre: ";
-        cin.getline(A[i].NOMBRE, 30);
+        printf("Nombre: ");
+        fgets(A[i].NOMBRE, 30, stdin);
+        A[i].NOMBRE[strcspn(A[i].NOMBRE, "\n")] = 0; // quitar salto de línea
 
         for (int j = 0; j < 5; j++) {
-            cout << "Calificación " << j + 1 << ": ";
-            cin >> A[i].CAL[j];
+            printf("Calificación %d: ", j + 1);
+            scanf("%f", &A[i].CAL[j]);
         }
-        cin.ignore();
+        getchar();
     }
     return 0;
 }
 
 void bubbleSort(ALUMNO A[], int T) {
-    bool swapped;
-    for (int i = 0; i < T - 1; i++) {
-        swapped = false;
-        for (int j = 0; j < T - i - 1; j++) {
+    int i, j;
+    int swapped;
+    for (i = 0; i < T - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < T - i - 1; j++) {
             if (strcmp(A[j].NOMBRE, A[j + 1].NOMBRE) > 0) {
-                swap(A[j], A[j + 1]);
-                swapped = true;
+                ALUMNO temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                swapped = 1;
             }
         }
         if (!swapped)
@@ -72,13 +75,13 @@ void bubbleSort(ALUMNO A[], int T) {
 
 void imprimir(ALUMNO A[], int T) {
     for (int i = 0; i < T; i++) {
-        cout << "\nAlumno " << i + 1 << ":\n";
-        cout << "Matrícula: " << A[i].MATRICULA << "\n";
-        cout << "Nombre: " << A[i].NOMBRE << "\n";
-        cout << "Calificaciones: ";
+        printf("\nAlumno %d:\n", i + 1);
+        printf("Matrícula: %d\n", A[i].MATRICULA);
+        printf("Nombre: %s\n", A[i].NOMBRE);
+        printf("Calificaciones: ");
         for (int j = 0; j < 5; j++) {
-            cout << A[i].CAL[j] << " ";
+            printf("%.2f ", A[i].CAL[j]);
         }
-        cout << "\n";
+        printf("\n");
     }
 }
